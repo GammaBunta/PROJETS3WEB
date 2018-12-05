332ad9a25fa131980c339adb2e45d46ac93b6c70
Sarah Brood <sarahbrood@gmail.com>
2018-12-05 13:04:38 +0100

    rename file

--- scripts/categorie.js
+++ scripts/categorie.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#lait").click(function(){
         document.getElementById("titre").innerHTML="Produits Laitiers";
-        $.get("./ajax/test.php", {famille : "Produits Laitiers"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Produits Laitiers"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -12,7 +12,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#viandes").click(function(){
         document.getElementById("titre").innerHTML="Viandes";
-        $.get("./ajax/test.php", {famille : "Viandes"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Viandes"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -23,7 +23,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#legumes").click(function(){
         document.getElementById("titre").innerHTML="Legumes";
-        $.get("./ajax/test.php", {famille : "Legumes"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Legumes"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -34,7 +34,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#fruits").click(function(){
         document.getElementById("titre").innerHTML="Fruits";
-        $.get("./ajax/test.php", {famille : "Fruits"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Fruits"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -45,7 +45,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#feculents").click(function(){
         document.getElementById("titre").innerHTML="Féculents";
-        $.get("./ajax/test.php", {famille : "Feculents"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Feculents"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -56,7 +56,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#condiments").click(function(){
         document.getElementById("titre").innerHTML="Condiments";
-        $.get("./ajax/test.php", {famille : "Condiments"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Condiments"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -67,7 +67,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#autres").click(function(){
         document.getElementById("titre").innerHTML="Autres";
-        $.get("./ajax/test.php", {famille : "Autre"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Autre"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
@@ -78,7 +78,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#epicerie").click(function(){
         document.getElementById("titre").innerHTML="Epicerie";
-        $.get("./ajax/test.php", {famille : "Epicerie"}).done(function(data){
+        $.get("./ajax/ajaxFrigo.php", {famille : "Epicerie"}).done(function(data){
             document.getElementById("listeIngr").innerHTML= data;
         });
     }
