c9be4e57c370805706e5b3985ee2bea46c8494fd
Sarah Brood <sarahbrood@gmail.com>
2019-01-03 18:54:50 +0100

    c'est long je vais boire une bière

--- modules/ajoutRecette/Modele_ajoutRecette.php
+++ modules/ajoutRecette/Modele_ajoutRecette.php
@@ -10,7 +10,17 @@
             $categorie = htmlspecialchars($_POST['categorie']);
             $niveau = htmlspecialchars($_POST['niveau']);
             $vegetarien = htmlspecialchars($_POST['optionsVege']);
+            if($vegetarien == 'option1'){
+                $vegetarien=0;
+            }else{
+                $vegetarien=1;
+            }
             $gluten = htmlspecialchars($_POST['optionsGluten']);
+            if($gluten=='option1'){
+                $gluten=0;
+            }else{
+                $gluten=1;
+            }
             $nbPers = htmlspecialchars($_POST['nbPers']);
             $tpsPrepa = htmlspecialchars($_POST['tpsPrepa']);
             $tpsRepos = htmlspecialchars($_POST['tpsRepos']);
@@ -19,9 +29,32 @@
             $quantites = explode(',',$_POST['listeQuantites']);
             $text = htmlspecialchars($_POST['texteRecette']);
 
+            $avis = NULL;
+            $cout = NULL;
+            $img = NULL;
+            $nbavis=NULL;
+            $req = self::$bdd -> prepare('INSERT INTO Recette (idrec,idUser,titre, nbpers, categorie, vegetarien, gluteenFree, avisInternaut, niveau, cout, tpsprepa, tpscuisson, tpsrepose, textrec, img, nombreAvis)
+            values (DEFAULT,:idUser,:titre,:nbpers,:categorie,:vegetarien,:gluteen,:avisInternaut,:niveau,:cout,:tpsprepa,:tpscuisson,:tpsrepos,:textrec,:img,:nombreAvis)');
+            $req -> bindParam(':idUser',$_SESSION['id']);
+            $req -> bindParam(':titre', $nomRecette);
+            $req -> bindParam(':nbpers',$nbPers);
+            $req -> bindParam(':categorie', $categorie);
+            $req -> bindParam(':vegetarien',$vegetarien);
+            $req -> bindParam(':gluteen',$gluten);
+            $req -> bindParam(':avisInternaut', $avis);
+            $req -> bindParam(':niveau',$niveau);
+            $req -> bindParam(':cout',$cout);
+            $req -> bindParam(':tpsprepa',$tpsPrepa);
+            $req -> bindParam(':tpscuisson', $tpsCuisson);
+            $req-> bindParam(':tpsrepos',$tpsRepos);
+            $req -> bindParam(':textrec',$text);
+            $req -> bindParam(':img',$img);
+            $req -> bindParam(':nombreAvis',$nbavis);
 
-            
-            //(`idrec`, `idingr`, `quantite`,`idUser`)
+            if(!$req){
+                echo "\nPDO::errorInfo():\n";
+                print_r(self::$bdd->errorInfo());
+            }
 
 
 
