3de919d5a2a0c2d53106e2176da4540e73eb5cce
Samuel <samuelmichel974@gmail.com>
2019-01-11 13:02:24 +0100

    nouveau header

--- composants/head.php
+++ composants/head.php
@@ -1,17 +1,8 @@
 <title>O•FR!GO</title>
-
-	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" type="text/css" href="./CSS/styles.css">
+<link rel="stylesheet" type="text/css" href="./CSS/styles.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
--- composants/header.php
+++ composants/header.php
@@ -1,25 +1,33 @@
 <header>
 	<nav class="navbar navbar-expand-lg navbar-success bg-success">
-		<a id="ofrigo" class="float-left ml-5" href="index.php"><img  src="./Images/logo_blanc.png" /><a>
-			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Features</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Pricing</a>
-					</li>
-				</ul>
-
-			</div>
-		</nav>
-	</header>
+		<div class="container-fluid">
+			<a id="ofrigo" class="navbar-brand" href="index.php" ><img  src="./Images/logo_blanc.png" width="40" height="40" alt="logofrigo"/></a>
+			<div class="float-left">
+			<ul class="nav navbar-nav">
+			  <li class="nav-item">
+			    <a class="nav-link text-light" href="index.php">Accueil</a>
+			  </li>
+			  <li class="nav-item dropdown">
+			    <a class="nav-link dropdown-toggle text-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Recettes</a>
+			    <div class="dropdown-menu">
+			      <a class="dropdown-item" href="#">Recettes disponibles</a>
+			      <a class="dropdown-item" href="#">Créer une recette</a>
+			    </div>
+			  </li>
+				<li class="nav-item">
+        	<a class="nav-link disabled" href="#">Frigo</a>
+      	</li>
+			</ul>
+		</div>
+			<ul class="nav navbar-nav navbar-right">
+	      <li><a class="text-white" href="#"><img class="mr-1" src="./Images/logo_connexion.png"width="30" height="30" alt="logoConnexion"/>Connexion</a></li>
+    	</ul>
+		</div>
+	</nav>
+</header>
 	<!-- fin header du site -->
 
-	
+
 
 <!---
 	<?php
