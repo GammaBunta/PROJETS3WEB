63c43341b3c3cb133bfa171b231721253b314842
Sarah Brood <sarahbrood@gmail.com>
2019-01-19 11:13:03 +0100

    ça sent la fin

--- ClassesGeneriques/modele_generique.php
+++ ClassesGeneriques/modele_generique.php
@@ -7,16 +7,8 @@ class ModeleGenerique{
     }
 
     public static function init(){
-/*        $b = 'mysql:host=localhost;dbname=ProjetFrigo';
-        $user = 'root';
-        $password = 'samsam974';
-        self::$bdd = new PDO($b, $user, $password);
-
-*/
-
-//Le array à la fin permet d'avoir les erreurs de mysql dans les requêtes et wallah c'est cool.
         $servername = "localhost";
-        self::$bdd = new PDO("mysql:host=$servername;dbname=ProjetFrigo", 'root', 'samsam974',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+        self::$bdd = new PDO("mysql:host=$servername;dbname=frigo", 'root', '!Minou240499!',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
     }
 
